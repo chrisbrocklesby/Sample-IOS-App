b0921cb3ad090033a22f45276c8436d2af88aae4
Chris Brocklesby <git@chrisbrocklesby.com>
2018-04-24 17:01:27 +0100

    updated

--- Sample iOS App/PostModel.swift
+++ Sample iOS App/PostModel.swift
@@ -1,14 +1,17 @@
-struct Root: Codable{
-	var data:[Data]
+// JSON (Decodable) Structure - Top Level
+struct Root: Decodable {
+	var data:[Data] // Set as Array
 }
 
-struct Data: Codable{
+// JSON (Decodable) Structure - Nested Data Values (Array)
+struct Data: Decodable {
 	let id: Int
 	let title: String
 	let body: String
 }
 
-struct Post: Codable{
+// Post (Codable) Structure - Uses Decoded JSON Structure
+struct Post: Codable {
 	let id: Int
 	let title: String
 	let body: String
